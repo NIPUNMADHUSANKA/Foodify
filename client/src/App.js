9bb0b3323a85fe2c29a8152afea2e8c71de9eafa
PiyumSankalpa <piyumsankalpaps@gmail.com>
2022-07-19 18:56:56 +0530

    Restaurant cover

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route , Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './assets/css/App.css';
+import Restaurant from './pages/Restaurant';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div>App</div>
+    <Box>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={ <Restaurant /> } />
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
 
