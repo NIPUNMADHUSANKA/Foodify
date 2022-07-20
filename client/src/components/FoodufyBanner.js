7da8dc4fa4bd6bee5cd009a5163abf794762959e
NIPUNMADHUSANKA <nipunmadhusanka201085@gmail.com>
2022-07-21 00:58:46 +0530

    Landing Page

--- client/src/components/FoodufyBanner.js
+++ client/src/components/FoodufyBanner.js
@@ -1,68 +1,75 @@
 import React from 'react'
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, ThemeProvider } from '@mui/material';
+
 import FOODIFY_BANNER from '../assets/images/foodify-banner.png';
 
-import { ThemeProvider, createTheme } from "@mui/material";
+import theme, { Colours } from './../assets/theme/theme';//to use theme provider,need to import this
 
-const theme = createTheme();
-
-theme.typography.h3 = {
-  fontSize: '3rem',
-  '@media (min-width:600px)': {
-    fontSize: '3.5rem',
-  },
-  [theme.breakpoints.up('md')]: {
-    fontSize: '4rem',
-  },
-};
 
 function FoodufyBanner() {
   return (
-    <Box sx={{mt:{lg:'150px', xs:'70px'},ml:{sm:'50px'}}}
-    position="relative" p="20px">
 
     <ThemeProvider theme={theme}>
+    
+    <Box sx={{mt:{lg:'12%', xs:'10%'},ml:{sm:'50px'}, 
+            [theme.breakpoints.down('sm')]: {
+              padding:"5%"
+            }}}
+    position="relative" p="20px">
 
-        <Typography color="#95CD41" fontWeight="350" variant="h3">
+        <Typography color="#95CD41" fontWeight="350" variant="h3" fontSize="390%"
+          sx= {{[theme.breakpoints.down('md')]: {
+            fontSize: '300%'
+          }}}
+        >
           Healthy Foods
         </Typography>
 
-        <Typography color="#FFFFFF" fontWeight="400" variant="h3">
+        <Typography color="#FFFFFF" fontWeight="400" variant="h3" fontSize="300%"
+          sx= {{[theme.breakpoints.down('md')]: {
+            fontSize: '250%'
+          }}}>
           Recommendation & <br /> Order Foods
         </Typography>
 
-    </ThemeProvider>
+        <Box sx={{mt:{lg:'5%', xs:'3%'}}}>
 
-        <Button variant='contained' 
-        style={{
-            borderRadius: 10,
-            backgroundColor: "#95CD41",
-            fontSize: "18px",
-            color:"#1B1B1E",
-            width:120
-         }}
-        >
-        SIGNIN
-        </Button>
+            <Button variant='contained' 
+            style={{
+                borderRadius: 10,
+                backgroundColor: "#95CD41",
+                fontSize: "18px",
+                color:"#1B1B1E",
+                width:120
+            }}
+            >
+            SIGNIN
+            </Button>
 
-        <Button variant='contained' 
-        style={{
-            borderRadius: 10,
-            backgroundColor: "#95CD41",
-            fontSize: "18px",
-            color:"#1B1B1E",
-            width:120,
-            
-         }}
-         sx={{ml:{lg:'15px', xs:'40px'}}}
-        >
-        SIGNUP
-        </Button>
+            <Button variant='contained' 
+            style={{
+                borderRadius: 10,
+                backgroundColor: "#95CD41",
+                fontSize: "18px",
+                color:"#1B1B1E",
+                width:120,
+                
+            }}
+            sx={{ml:{lg:'15px', xs:'40px'},
+            [theme.breakpoints.down('xs')]: {
+              ml:{lg:"80%"}
+            }}}
+            >
+            SIGNUP
+            </Button>
 
-        <img src={FOODIFY_BANNER} alt="banner" className='hero-banner-img' />
-
-       
+         </Box>
     </Box>
+
+    <img src={FOODIFY_BANNER} alt="banner" className='hero-banner-img' />
+
+
+    </ThemeProvider>
   )
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,15 +3,23 @@ import { Box } from '@mui/material';
 
 import FoodBanner from '../components/FoodufyBanner';
 import HorizontalBar from '../components/HorizontalBar';
-import AboutUs from '../components/AboutUs';
+import AboutUs from '../components/AboutUs2';
 import ContactUs from '../components/ContactUs';
+
+import FOODIFY_ABOUTUS from '../assets/images/golden-cutlery.jpg';
+
+const details = {
+  "detail1" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+  "detail2" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+  "detail3" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+}
 
 function Landing() {
   return (
     <Box>
         <FoodBanner />
         <HorizontalBar />
-        <AboutUs />
+        <AboutUs AboutImage = {FOODIFY_ABOUTUS}  details = {details} />
         <ContactUs />
        
     </Box>
