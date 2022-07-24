ca478346d022611bf2367bdce3011be907f7ad2c
988451835v <Kadnishedha@gmail.com>
2022-07-24 23:13:29 +0530

    Completed Profile_edit

--- client/src/components/profile/AddressForm.js
+++ client/src/components/profile/AddressForm.js
@@ -7,23 +7,84 @@ import Checkbox from '@mui/material/Checkbox';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-import theme, { Colours } from '../../assets/theme/theme';//to use theme provider,need to import this
+import theme, { Colours } from '../../assets/theme/theme';
+//to use theme provider,need to import this
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import FolderIcon from '@mui/icons-material/Folder';
 
 
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+////////////////////////////////////////////input deseases
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+  },
+  { title: 'The Good, the Bad and the Ugly', year: 1966 },
+  { title: 'Fight Club', year: 1999 },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: 2001,
+  },
+  {
+    title: 'Star Wars: Episode V - The Empire Strikes Back',
+    year: 1980,
+  },
+  { title: 'Forrest Gump', year: 1994 },
+  { title: 'Inception', year: 2010 },
+  {
+    title: 'The Lord of the Rings: The Two Towers',
+    year: 2002,
+  },
+  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+  { title: 'Goodfellas', year: 1990 },
+  { title: 'The Matrix', year: 1999 },
+  { title: 'Seven Samurai', year: 1954 },
+  {
+    title: 'Star Wars: Episode IV - A New Hope',
+    year: 1977,
+  },
+  { title: 'City of God', year: 2002 },
+  { title: 'Se7en', year: 1995 },
+  { title: 'The Silence of the Lambs', year: 1991 },
+  { title: "It's a Wonderful Life", year: 1946 },
+  { title: 'Life Is Beautiful', year: 1997 },
+  { title: 'The Usual Suspects', year: 1995 },
+  { title: 'Léon: The Professional', year: 1994 },
+  { title: 'Spirited Away', year: 2001 },
+  { title: 'Saving Private Ryan', year: 1998 },
+  { title: 'Once Upon a Time in the West', year: 1968 },
+  { title: 'American History X', year: 1998 },
+  { title: 'Interstellar', year: 2014 },
+];
+
+////////////////////////////////////////////////////////////
 
 export default function AddressForm() {
   return (
     <Container 
     sx={{
-      backgroundColor:'#FFFFFF' ,
+      backgroundColor:Colours.dark ,
       width:'60%',
-      
+      marginTop:'4%',
+      marginBottom:'4%',
+      borderRadius:"33px" 
       }}>
-     
-      {/* <Typography variant="h6" gutterBottom >
-        Profile
-      </Typography> */}
-      <Box sx={{paddingTop:'2%'}}>
+    
+      <Box sx={{paddingTop:'2%'}} >
       <Avatar
         alt="Remy Sharp"
         src="/static/images/avatar/1.jpg"
@@ -33,12 +94,31 @@ export default function AddressForm() {
          margin:'auto',
          }}
          
+         
       />
+      <Box sx={{position:'absolute',marginLeft:'30%',marginTop:'-2%'}}>
+      <Stack>
+      <Avatar>
+        <FolderIcon />
+      </Avatar>
+      </Stack>
       </Box>
-
-      <Grid container spacing={3}>
+      </Box>
+          
+      <Typography sx={{marginLeft:"1%",marginBottom:"5%", marginTop:"2%",color:Colours.formWhite,fontWeight:''}} variant="h6" gutterBottom >
+        Profile Details
+      </Typography>
+      <Grid container spacing={3} sx={{color:Colours.formWhite}} >
         <Grid item xs={12} sm={6}>
           <TextField
+          sx={{ input: 
+            { color: Colours.formWhite }, 
+            "label": {color: Colours.formWhite},
+            "& label.Mui-focused": {
+            color:Colours.formWhite
+            },backgroundColor:Colours.transparenceGrey
+        
+            }}
             required
             id="firstName"
             name="firstName"
@@ -47,10 +127,19 @@ export default function AddressForm() {
             autoComplete="given-name"
             defaultValue="Riyansa"
             variant="filled"
+           
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
+            sx={{ input: 
+            { color: Colours.formWhite }, 
+            "label": {color: Colours.formWhite},
+            "& label.Mui-focused": {
+            color:Colours.formWhite
+            },backgroundColor:Colours.transparenceGrey
+        
+            }}
             id="lastName"
             name="lastName"
             label="Last Name"
@@ -59,9 +148,76 @@ export default function AddressForm() {
             defaultValue="Abesinghe"
             variant="filled"
           />
+          
+        </Grid>
+        <Grid item xs={12}>
+         
+           <TextField
+           sx={{ input: 
+            { color: Colours.formWhite }, 
+            "label": {color: Colours.formWhite},
+            "& label.Mui-focused": {
+            color:Colours.formWhite
+            },backgroundColor:Colours.transparenceGrey
+        
+            }}
+            id="Email"
+            name="Email"
+            label="Email"
+            fullWidth
+            autoComplete="email"
+            defaultValue="RiyansaAbeweera@gmail.com"
+            variant="filled"
+          />
         </Grid>
         <Grid item xs={12}>
           <TextField
+            sx={{ input: 
+              { color: Colours.formWhite }, 
+              "label": {color: Colours.formWhite},
+              "& label.Mui-focused": {
+              color:Colours.formWhite
+              },backgroundColor:Colours.transparenceGrey
+          
+              }}
+            id="Telephone no"
+            name="Telephone no"
+            label="Telephone no"
+            fullWidth
+            autoComplete="Telephone no"
+            defaultValue="0701234567"
+            variant="filled"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            sx={{ input: 
+              { color: Colours.formWhite }, 
+              "label": {color: Colours.formWhite},
+              "& label.Mui-focused": {
+              color:Colours.formWhite
+              },backgroundColor:Colours.transparenceGrey
+          
+              }}
+            id="City"
+            name="City"
+            label="City"
+            fullWidth
+            autoComplete="City"
+            defaultValue="Kurunegala"
+            variant="filled"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <TextField
+            sx={{ input: 
+              { color: Colours.formWhite }, 
+              "label": {color: Colours.formWhite},
+              "& label.Mui-focused": {
+              color:Colours.formWhite
+              },backgroundColor:Colours.transparenceGrey
+          
+              }}
             id="hometown"
             name="hometown"
             label="Home Town"
@@ -71,64 +227,154 @@ export default function AddressForm() {
             variant="filled"
           />
         </Grid>
+
+        {/* health conditions */}
         <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-          />
+        <Typography sx={{marginLeft:"1%"}} variant="h6" gutterBottom >
+        Health Status
+        </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6} >
+        <Typography sx={{marginLeft:"1%"}} variant="h6" gutterBottom >
+        Diseases
+        </Typography>
+         <Typography sx={{marginLeft:"2%",backgroundColor:Colours.transparenceGrey,paddingLeft:'2%'}} >
+          <p>Diabetic</p>
+          <p>Diabetic</p>
+          <p>Diabetic</p>
+          <p>Diabetic</p>
+          </Typography>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
+        <Typography sx={{marginLeft:"1%"}} variant="h6" gutterBottom >
+        Add Disease
+        </Typography  >
+          <Autocomplete
+            multiple
+            id="checkboxes-tags-demo"
+            options={top100Films}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option.title}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                  
+                />
+                {option.title}
+              </li>
+            )}
+            style={{ width: '100%',backgroundColor:Colours.transparenceGrey }}
+            renderInput={(params) => (
+              <TextField {...params} sx={{  input: 
+                { color: Colours.formWhite }, 
+                "label": {color: Colours.formWhite},
+                "& label.Mui-focused": {
+                color:Colours.formWhite
+                }, 
+                
+                }} label="Checkboxes" placeholder="deseases" />
+            )}
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid>
+
+        {/*food preferences */}
         <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
+        <Typography sx={{marginLeft:"1%"}} variant="h6" gutterBottom >
+        Food Preferences
+        </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6} >
+        <Typography sx={{marginLeft:"1%"}} variant="h6" gutterBottom >
+        Like Foods
+        </Typography>
+         <Typography sx={{marginLeft:"2%",backgroundColor:Colours.transparenceGrey,paddingLeft:"2%"}} >
+          <p>Pizza</p>
+          <p>Veg soup</p>
+          <p>Pizza</p>
+          <p>Veg soup</p>
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <Typography sx={{marginLeft:"1%"}} variant="h6" gutterBottom >
+        Add Foods
+        </Typography>
+          <Autocomplete
+            multiple
+            id="checkboxes-tags-demo"
+            options={top100Films}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option.title}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8,color: "#00e676" }}
+                  checked={selected}
+                />
+                {option.title}
+              </li>
+            )}
+            style={{ width: '100%', backgroundColor:Colours.transparenceGrey }}
+            renderInput={(params) => (
+              <TextField sx={{  input: 
+                { color: Colours.formWhite }, 
+                "label": {color: Colours.formWhite},
+                "& label.Mui-focused": {
+                color:Colours.formWhite
+                }, 
+                
+                }}
+              {...params} label="Checkboxes" placeholder="Favorites" />
+            )}
           />
         </Grid>
+        <Grid item xs={12} sm={6} marginBottom={'2%'}>
+        <Button xs={12} sm={6} href='#' variant="contained" sx={{
+             marginLeft:'75%',
+              background: Colours.green, '&:hover': {
+                backgroundColor: Colours.yellow,
+              },
+              color: Colours.dark,
+              fontSize: '20px',
+              hover: Colours.yellow,
+              [theme.breakpoints.down('sm')]: {
+                fontSize: '18px',
+                marginLeft:'0%',
+                width:'100%',
+
+               
+              },
+            }}>
+              SAVE
+            </Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <Button xs={12} sm={6} href='#' variant="contained" sx={{
+              
+              background: Colours.darkgray, '&:hover': {
+                backgroundColor: Colours.grayWhite, color: Colours.dark,
+              },
+              color: Colours.grayWhite,
+              fontSize: '20px',
+              [theme.breakpoints.down('sm')]: {
+                fontSize: '18px',
+                width:'100%',
+                marginBottom:'7%',
+                
+              },
+            }}>
+              CANCEL
+            </Button>
+          
+        </Grid>
+       
       </Grid>
     </Container>
   );
--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -6,15 +6,14 @@ import '../assets/css/Profile.css';
 import Pnavbar from '../components/profile/profileNavbar';
 import Pcarousel from '../components/profile/profileCarouselGood';
 import ProfileDetails from '../components/profile/profile_details';
-import profileForm from '../components/profile/profile_form'
-import CheckoutW from '../components/profile/Checkout';
 import AddressFormW from '../components/profile/AddressForm';
+import '../assets/css/Profile.css';
 
 const Profile = () => {
 
 
   return (
-    <Box marginTop="0px">
+    <Box className='Profile2-body-Style' marginTop="0px">
       <Pnavbar/>
       <AddressFormW/>
       
