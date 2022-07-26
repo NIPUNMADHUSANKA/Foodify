a92c81c912a42ee16918d2dda17a8c1809c14e5c
988451835v <Kadnishedha@gmail.com>
2022-07-27 00:09:25 +0530

    Pickup-first

--- client/src/pages/UserOrder.js
+++ client/src/pages/UserOrder.js
@@ -3,11 +3,6 @@ import { useEffect } from 'react';
 import { Box } from '@mui/material';
 import theme, { Colours } from '../assets/theme/theme'; //to use theme provider,need to import this
 import '../assets/css/Profile.css';
-import Pnavbar from '../components/profile/profileNavbar';
-import Pcarousel from '../components/profile/profileCarouselGood';
-import AddressFormW from '../components/profile/AddressForm';
-import '../assets/css/Profile.css';
-import UserOrderd from '../components/restaurant_order/user_oreder_details';
 import Unavbar from '../components/user_order/user_order_navbar';
 import UserOrderBanner from '../components/user_order/user_order_upper';
 const RestaurantOrder = () => {
