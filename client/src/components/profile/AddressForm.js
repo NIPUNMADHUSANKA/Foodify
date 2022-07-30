77ca6211378a15c44a77f88cdf9b312ee9cf2030
988451835v <Kadnishedha@gmail.com>
2022-07-31 02:15:10 +0530

    Completion Login

--- client/src/components/profile/AddressForm.js
+++ client/src/components/profile/AddressForm.js
@@ -107,7 +107,7 @@ export default function AddressForm() {
         Profile Details
       </Typography>
       <Grid container spacing={3} sx={{color:Colours.formWhite}} >
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} >
           <TextField
           sx={{ input: 
             { color: Colours.formWhite }, 
@@ -118,9 +118,9 @@ export default function AddressForm() {
         
             }}
             required
-            id="firstName"
-            name="firstName"
-            label="First Name"
+            id="userName"
+            name="userName"
+            label="Username"
             fullWidth
             autoComplete="given-name"
             defaultValue="Riyansa"
@@ -128,7 +128,7 @@ export default function AddressForm() {
            
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
+        {/* <Grid item xs={12} sm={6}>
           <TextField
             sx={{ input: 
             { color: Colours.formWhite }, 
@@ -147,7 +147,7 @@ export default function AddressForm() {
             variant="filled"
           />
           
-        </Grid>
+        </Grid> */}
         <Grid item xs={12}>
          
            <TextField
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography, Button, ThemeProvider, Stack } from '@mui/material';
 
 import LoginBanner from '../components/login/LoginBanner';
 import HorizontalBar from '../components/HorizontalBar';
@@ -7,6 +7,13 @@ import LoginNavbar from '../components/login/LoginNavbar';
 import '../assets/css/Login.css';
 import theme, { Colours } from '../assets/theme/theme';
 import Skeleton from '@mui/material/Skeleton';
+import Reveal from 'react-reveal/Reveal';
+import Zoom from '@mui/material/Zoom'; //Zoom text
+import Flip from 'react-reveal/Flip';
+import Slide from 'react-reveal/Slide';
+import { Paper} from '@mui/material';
+
+
 
 
 
@@ -14,10 +21,79 @@ import Skeleton from '@mui/material/Skeleton';
 function Login() {
   return (
     <Box className='Login-body-Style' >
-    
-    
-        <LoginNavbar/>
-        <LoginBanner />
+       <LoginNavbar/>
+
+
+
+       <Box maxWidth='100%' sx={{
+        padding: '0',
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+      }}>
+        {/* ------------------Login Part------------------------------- */}
+
+        {/* Box 1 for Login */}
+       <LoginBanner/>
+        {/* Box 1 */}
+
+
+
+
+        {/* Box 2 */}
+        <Box sx={{
+         
+          height: 'auto',
+          width:'100%',
+          justifyContent: 'left',
+
+          [theme.breakpoints.down('sm')]: {
+            fontSize: '8px',
+            padding: '2px',
+            width: '100%',
+          }
+        }}>
+         
+           
+              <Zoom in={true}>
+                  
+                  <Typography color="#95CD41" fontWeight="400" variant="h2" fontSize="500%"
+                    sx= {{
+                      marginTop: '20%',
+                      marginLeft:'14%',
+                     
+                      [theme.breakpoints.down('md')]: {
+                      fontSize: '350%',
+                      
+                    },
+                    [theme.breakpoints.down('sm')]: {
+                      fontSize: '230%'
+                    }
+                  }}
+                  >
+                    <Slide right>
+                    Eat Healthily With
+                    </Slide>
+                    
+                    <Slide left>
+                   
+                    Your  Prefer
+                  
+                    </Slide>
+                   </Typography>
+                  
+            </Zoom>
+            <Typography>
+                   <Skeleton 
+                      sx={{ backgroundColor:Colours.transparenceGrey, marginLeft:'55%'}}
+                      variant="circular" width={140} height={140}/>
+                  </Typography>
+          
+        
+        </Box>
+        {/* Box 2 */}
+
+      </Box>    
         
       
     </Box>
