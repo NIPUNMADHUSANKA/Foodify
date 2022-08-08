191a9b822c00293742d78206ada70eded5d72ec2
PiyumSankalpa <piyumsankalpaps@gmail.com>
2022-08-08 19:30:32 +0530

    fixed some eroors

--- Backend/src/main/java/Foodify/Backend/repository/RestaurantRepository.java
+++ Backend/src/main/java/Foodify/Backend/repository/RestaurantRepository.java
@@ -1,4 +1,4 @@
-package Foodify.Backend.Repository;
+package Foodify.Backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
