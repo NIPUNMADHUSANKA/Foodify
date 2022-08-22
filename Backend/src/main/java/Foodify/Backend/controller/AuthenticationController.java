53fd53b167f2479833a427a0fb587082c3ff76ae
988451835v <Kadnishedha@gmail.com>
2022-08-22 14:59:51 +0530

    Authentication Update

--- Backend/src/main/java/Foodify/Backend/controller/AuthenticationController.java
+++ Backend/src/main/java/Foodify/Backend/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
