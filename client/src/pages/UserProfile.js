01e290c029e26fada4c455643ba60a0d34db764e
988451835v <Kadnishedha@gmail.com>
2022-08-23 13:37:05 +0530

    Creating a table for view food items and restaurants

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,21 +1,44 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import theme, { Colours } from '../assets/theme/theme'; //to use theme provider,need to import this
 import '../assets/css/Profile.css';
 import Pnavbar from '../components/Navbar';
 import Pcarousel from '../components/profile/profileCarouselGood';
+import Fooditemstable from '../components/profile/VieweatenFooditems';
 
 
 const ProfileD = () => {
 
 
   return (
-    <Box className='Profile-body-Style' marginTop="0px" >
+    <Box className='Profile-body-Style' marginTop="0px" sx={{overflow:"visible"}}>
       <Pnavbar />
       <Pcarousel/>
-     
+      <Box>
+      <Typography variant="h3" gutterBottom component="div" sx={{
+        color: Colours.grayWhite,
+        ml:"4%",
+        mt:"3%",
+
+        // Changing font family 
+        fontFamily:"Poppins",
+        [theme.breakpoints.down('sm')]: {
+          fontSize: '220%',
+          mt:"5%",
+          ml:"5%"
+        }
+      }}>Orders</Typography>
+        <Box>
+          <Fooditemstable/>
+        </Box>
+
+      </Box>
+
     </Box>
+
+     
+   
 
     
   )
