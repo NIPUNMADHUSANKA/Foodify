f5f2ad053656fe2571f924b21ee3d4777366a965
PiyumSankalpa <piyumsankalpaps@gmail.com>
2022-08-24 05:51:02 +0530

    fixed some errors in restaurant pages

--- client/src/components/restaurant/profile/RestaurantHome.js
+++ client/src/components/restaurant/profile/RestaurantHome.js
@@ -14,11 +14,18 @@ import Map from '../../../assets/images/GoogleMapTA.webp';
 import Fade from 'react-reveal/Fade';
 
 // to collect the description imformation
+// const details = {
+//   "detail1" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+//   "detail2" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+//   "detail3" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+// }
+
+// to collect the description imformation
 const details = {
-  "detail1" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
-  "detail2" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
-  "detail3" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
-}
+    "detail1": "One of Sri Lankan's most beloved restaurants,",
+    "detail2": "Bojun Hut has welcomed guests to enjoy its contemporary Sri Lankan cuisine, warm hospitality, and unparalleled service in Colombo City for over two decades.",
+    "detail3": " Chef Michael Anthony's ever-evolving seasonal menu showcases the restaurant's relationships with local farms and purveyors.",
+  }
 
 const contactdetails = {
   "Location":"Location",
@@ -30,6 +37,29 @@ const comments = {
   "name":"username",
   "detail1" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
 }
+
+const comments1 = [
+    {
+      "name": "Navod",
+      "detail1": "This is a good restaurant",
+    },
+    {
+      "name": "Sankalpa98",
+      "detail1": "This place has good hospitality",
+    },
+    {
+      "name": "Nisheda",
+      "detail1": "This place provides good service",
+    },
+    {
+      "name": "Shehan21",
+      "detail1": "This offers food with exelent quality",
+    },
+    {
+      "name": "Jane",
+      "detail1": "This is a good restaurant",
+    }
+  ]
 
 const RestaurantHome = () => {
     return (
@@ -48,7 +78,7 @@ const RestaurantHome = () => {
             </Fade>
 
             <Fade big>
-                <RestaurantComment comments={comments} />
+                <RestaurantComment comments={comments1} />
             </Fade>
 
             <Fade left>
