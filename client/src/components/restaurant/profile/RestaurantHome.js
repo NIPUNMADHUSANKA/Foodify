617122cabf07ae279c410df2c585d60f3fabec80
PsSankalpa <piyumsankalpaps@gmail.com>
2022-08-30 08:16:28 +0530

    restaurant details and images adding completed

--- client/src/components/restaurant/profile/RestaurantHome.js
+++ client/src/components/restaurant/profile/RestaurantHome.js
@@ -1,11 +1,14 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import EditAboutUs from './EditAboutUs';
 import RestaurantOffers from '../RestaurantOffers';
 import RestaurantMenu from '../RestaurantMenu';
 import RestaurantEditContact from './RestaurantEditContact';
 import RestaurantComment from '../RestaurantComments';
+
+import axios from 'axios';
+import authHeader from "../../../services/auth-header";
 
 import AboutImage from '../../../assets/images/golden-cutlery.jpg';//exporting the image for about section
 import Map from '../../../assets/images/GoogleMapTA.webp';
@@ -22,71 +25,113 @@ import Fade from 'react-reveal/Fade';
 
 // to collect the description imformation
 const details = {
-    "detail1": "One of Sri Lankan's most beloved restaurants,",
-    "detail2": "Bojun Hut has welcomed guests to enjoy its contemporary Sri Lankan cuisine, warm hospitality, and unparalleled service in Colombo City for over two decades.",
-    "detail3": " Chef Michael Anthony's ever-evolving seasonal menu showcases the restaurant's relationships with local farms and purveyors.",
-  }
+  "detail1": "One of Sri Lankan's most beloved restaurants,",
+  "detail2": "Bojun Hut has welcomed guests to enjoy its contemporary Sri Lankan cuisine, warm hospitality, and unparalleled service in Colombo City for over two decades.",
+  "detail3": " Chef Michael Anthony's ever-evolving seasonal menu showcases the restaurant's relationships with local farms and purveyors.",
+}
 
 const contactdetails = {
-  "Location":"Location",
-  "Address":"Address",
-  "TpNumber":"Tp Number",
+  "Location": "Location",
+  "Address": "Address",
+  "TpNumber": "Tp Number",
 }
 
 const comments = {
-  "name":"username",
-  "detail1" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
+  "name": "username",
+  "detail1": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eumquasi quidem quibusdam.",
 }
 
 const comments1 = [
-    {
-      "name": "Navod",
-      "detail1": "This is a good restaurant",
-    },
-    {
-      "name": "Sankalpa98",
-      "detail1": "This place has good hospitality",
-    },
-    {
-      "name": "Nisheda",
-      "detail1": "This place provides good service",
-    },
-    {
-      "name": "Shehan21",
-      "detail1": "This offers food with exelent quality",
-    },
-    {
-      "name": "Jane",
-      "detail1": "This is a good restaurant",
-    }
-  ]
+  {
+    "name": "Navod",
+    "detail1": "This is a good restaurant",
+  },
+  {
+    "name": "Sankalpa98",
+    "detail1": "This place has good hospitality",
+  },
+  {
+    "name": "Nisheda",
+    "detail1": "This place provides good service",
+  },
+  {
+    "name": "Shehan21",
+    "detail1": "This offers food with exelent quality",
+  },
+  {
+    "name": "Jane",
+    "detail1": "This is a good restaurant",
+  }
+]
 
 const RestaurantHome = () => {
-    return (
-        <Box>
 
-            <Fade left>
-                <EditAboutUs AboutImage={AboutImage} details={details} />
-            </Fade>
+  const [AboutUs, setAboutUs] = useState(null);
 
-            <Fade right>
-                <RestaurantOffers />
-            </Fade>
+  const [ContactDetails, setContactDetails] = useState(null);
 
-            <Fade bottom>
-                <RestaurantMenu />
-            </Fade>
+  // ---------------for get restaurant data------------------------
+  useEffect(() => {
 
-            <Fade big>
-                <RestaurantComment comments={comments1} />
-            </Fade>
+    axios.get("http://localhost:8072/FoodiFy/Restaurant/GetRestaurantInfo", { headers: authHeader() })
+      .then(data => {
+        console.log(data)
 
-            <Fade left>
-                <RestaurantEditContact Map={Map} details={contactdetails} />
-            </Fade>
+        if(data.data.about !== null){
 
-        </Box>
-    )
+          const details1 = {
+            "detail1": data.data.about,
+            "detail2": "",
+            "detail3": "",
+          }
+
+          setAboutUs(details1)
+        }
+        
+
+        
+        if(data.data.location !== null && data.data.address !== null && data.data.telephone !== null){
+
+          const contactdetails1 = {
+            "Location": data.data.location,
+            "Address": data.data.address,
+            "TpNumber": data.data.telephone,
+          }
+  
+          setContactDetails(contactdetails1)
+          
+        }
+        
+  
+      }).catch(err => console.log(err));
+  }, []);
+
+
+  return (
+    <Box>
+
+      <Fade left>
+        <EditAboutUs AboutImage={AboutImage} details={AboutUs !== null ? AboutUs : details} />
+      </Fade>
+
+      <Fade right>
+        <RestaurantOffers />
+      </Fade>
+
+      <Fade bottom>
+        <RestaurantMenu />
+      </Fade>
+
+      <Fade big>
+        <RestaurantComment comments={comments1} />
+      </Fade>
+
+      <Fade left>
+        <RestaurantEditContact Map={Map} details={ContactDetails !== null ? ContactDetails : contactdetails} />
+      </Fade>
+
+    </Box>
+  )
 }
 
 export default RestaurantHome
