777f21663d5234e430a65cf75449d29167c37951
988451835v <Kadnishedha@gmail.com>
2022-09-11 20:02:56 +0530

    Completing complain controller

--- Backend/src/main/java/Foodify/Backend/controller/ComplainController.java
+++ Backend/src/main/java/Foodify/Backend/controller/ComplainController.java
@@ -1,7 +1,6 @@
 package Foodify.Backend.controller;
 
 import java.io.IOException;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,11 +10,9 @@ import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-=======
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> c48c6e329bcbfa925358786357726bdba0f706a1
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
