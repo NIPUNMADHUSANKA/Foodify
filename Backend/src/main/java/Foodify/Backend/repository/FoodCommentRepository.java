bcae955b6cca56141997a067bc8157b80ee830df
Yohombu Abeysinghe <yohombuda@gmail.com>
2022-10-24 17:27:47 +0530

    food rating backend

--- Backend/src/main/java/Foodify/Backend/repository/FoodCommentRepository.java
+++ Backend/src/main/java/Foodify/Backend/repository/FoodCommentRepository.java
@@ -1,11 +1,18 @@
 package Foodify.Backend.repository;
 
+import Foodify.Backend.model.RestaurantComments;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import Foodify.Backend.model.FoodComment;
+import Foodify.Backend.model.FoodComments;
+
+import java.util.List;
 
 @Repository
-public interface FoodCommentRepository extends MongoRepository<FoodComment,String> {
+public interface FoodCommentRepository extends MongoRepository<FoodComments,String> {
+    FoodComments findByid(String id);
 
+    FoodComments findByfoodId(String foodId);
+
+    List<FoodComments> findByuserName(String userName);
 }
