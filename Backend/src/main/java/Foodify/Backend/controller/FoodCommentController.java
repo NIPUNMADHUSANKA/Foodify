b04af86587039ed9d173fbba2f6392f09c37e027
Yohombu Abeysinghe <yohombuda@gmail.com>
2022-10-26 12:55:13 +0530

    food comments done

--- Backend/src/main/java/Foodify/Backend/controller/FoodCommentController.java
+++ Backend/src/main/java/Foodify/Backend/controller/FoodCommentController.java
@@ -1,6 +1,8 @@
 package Foodify.Backend.controller;
 
+import Foodify.Backend.model.FoodItem;
 import Foodify.Backend.model.RestaurantComments;
+import Foodify.Backend.repository.FoodItem_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -16,21 +18,31 @@ public class FoodCommentController {
 	
 	@Autowired
 	private FoodCommentRepository foodCommentRepository;
+
+	@Autowired
+	private FoodItem_Repository foodItem_repository;
 	
 	@PostMapping("/FoodiFy/User/addFoodComment")
 	public FoodComments createComment(@RequestBody FoodComments Foodcomment) {
 		
 		String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-		System.out.println(Foodcomment);
-
+		System.out.println(Foodcomment.getFoodId());
+		FoodItem fooditem = foodItem_repository.findByid(Foodcomment.getFoodId());
 		System.out.println(userName);
 //		String restauratId=commentRes.getRestaurantId();
 //		String commentDescription=complain.getCommentDescription();
 //		Date addedDate=complain.getAddedDate();
 		
 		Foodcomment.setUsername(userName);
-		
-		
+		Double rating1 = Foodcomment.getFoodRating();
+		Double rating2 = fooditem.getFoodRating();
+		if(rating2==null){
+			rating2=rating1;
+		}
+		Double rating = (rating1 + rating2)/2;
+
+		fooditem.setFoodRating(rating);
+		foodItem_repository.save(fooditem);
 		return foodCommentRepository.save(Foodcomment);
 	}
 
--- Backend/src/main/java/Foodify/Backend/repository/FoodCommentRepository.java
+++ Backend/src/main/java/Foodify/Backend/repository/FoodCommentRepository.java
@@ -1,6 +1,5 @@
 package Foodify.Backend.repository;
 
-import Foodify.Backend.model.RestaurantComments;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
